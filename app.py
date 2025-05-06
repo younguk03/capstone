a4d4b518a7e747eb509a49e4a93ccb383f17e91e
younguk <0622ook@gmail.com>
2025-05-06 21:59:33 +0900

    first

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:////대학/3학년/1학기/캡스톤디자인/project/capstone/form.db')
+db = SQLAlchemy(app)
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20))
+    originalPage = db.Column(db.String(80))
+    testURL = db.Column(db.String(80))
+    payload = db.Column(db.String(80))
+
+
+@app.route('/', methods=['POST', 'GET'])
+def form():
+    return render_template('./home.html')
+
+
+@app.route('/templates/resultPage', methods=['POST', 'GET'])
+def result_page():
+    url = request.args.get('url', '')
+    lists = List.query.all()
+    return render_template('resultPage/page.html', url=url, lists=lists)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
