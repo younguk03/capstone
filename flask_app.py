1f02c16315cb7aea00c4548ff027b6f847011777
younguk03 <145085881+younguk03@users.noreply.github.com>
2025-05-12 14:21:45 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -37,4 +37,4 @@ def result_page():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT,10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",10000)))
