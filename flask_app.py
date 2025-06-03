df753b4453f1c595f377846d82ded964a03959aa
younguk03 <145085881+younguk03@users.noreply.github.com>
2025-06-03 19:21:47 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -75,4 +75,4 @@ def download_report():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",10000)))
