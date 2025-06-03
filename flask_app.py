14076d0bfa6f781d9750338f3fc98b0c9b897614
younguk <0622ook@gmail.com>
2025-06-03 19:53:38 +0900

    beautifulsoup4

--- flask_app.py
+++ flask_app.py
@@ -74,5 +74,5 @@ def download_report():
     return send_file(pdf_path, as_attachment=True)
 
 
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
